bd39e561d05eec07a6a744311b106b78fbd40583
jspark7213 <jspark72123@gmail.com>
2025-07-17 22:34:39 +0900

    fixing files of bucket name

--- src/app/routers/files.py
+++ src/app/routers/files.py
@@ -14,7 +14,6 @@ s3_client = boto3.client(
     region_name=os.getenv('AWS_REGION'),
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    S3_BUCKET=os.getenv('S3_BUCKET')
 )
 #pydantic 모델의 요청 형식 정의
 class FileUploadRequest(BaseModel):  
@@ -28,7 +27,7 @@ async def upload_file(file: FileUploadRequest):
         url = s3_client.generate_presigned_url(    
             'put_object', # S3에 파일 업로드 명령어
             Params={ #버킷 파라미터
-                'Bucket':file.S3_BUCKET,
+                'Bucket':os.getenv('S3_BUCKET'),
                 'Key': key,
                 'ContentType': file.filetype
             },
@@ -36,7 +35,7 @@ async def upload_file(file: FileUploadRequest):
         )
         return {
             "upload_url": url, # presigned URL 반환
-            "file_url": f"https://{file.S3_BUCKET}.s3.amazonaws.com/{key}"
+            "file_url": f"https://{os.getenv('S3_BUCKET')}.s3.amazonaws.com/{key}"
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))  # 에러 발생 시 500 에러 반환
