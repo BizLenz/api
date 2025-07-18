ff93149a6da1ffd541d7612f7d340e9c500c150b
jspark7213 <jspark72123@gmail.com>
2025-07-18 22:02:25 +0900

    Debugging(Add Library ValidaitonError)

--- src/app/routers/files.py
+++ src/app/routers/files.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from fastapi import FastAPI, HTTPException
 from botocore.exceptions import ClientError
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, ValidationError
 from dotenv import load_dotenv
 
 load_dotenv()
