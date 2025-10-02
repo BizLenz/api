ac9950c9129c8b47d3705edb501877f816ddf6f0
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-10-02 10:25:51 +0900

    fix(db): update to match erd

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -23,13 +23,14 @@ from app.database import Base
 # -----------------------
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)  # 사용자 ID
-    username = Column(String(50), unique=True, nullable=False)  # 사용자명 (UNIQUE)
-    password_hash = Column(String(255), nullable=False)  # 해시된 비밀번호
-    email = Column(String(255), unique=True)  # 이메일 (UNIQUE)
-    phone_number = Column(String(20))  # 연락처
-    address = Column(Text)  # 주소
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())  # 생성일시
+    id = Column(
+        String(255), primary_key=True, comment="Cognito Sub (서비스 내부 고유 ID)"
+    )
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        comment="서비스 프로필 생성 일시",
+    )
     updated_at = Column(
         TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now()
     )  # 수정일시
